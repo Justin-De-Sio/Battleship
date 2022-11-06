fc4786a6f23b9155489e216d9ec9b6c1123179ce
emile <emilebleicher14@gmail.com>
2022-11-06 12:10:40 +0100

    Changement ship: - créationde la méthode dellcooardinate(elle clear l'array list coordinate util pour remove le ship lorsque lon change de positon)

--- model/ship/Ship.java
+++ model/ship/Ship.java
@@ -1,6 +1,7 @@
 package model.ship;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public abstract class Ship {
 
@@ -22,6 +23,9 @@ public abstract class Ship {
     public void addCoordinates(int x, int y) {
         int[] coordinate = {x, y};
         coordinates.add(coordinate);
+    }
+    public void dellCooardinates(){
+        coordinates.clear();
     }
 
     public ArrayList<int[]> getCoordinates() {
