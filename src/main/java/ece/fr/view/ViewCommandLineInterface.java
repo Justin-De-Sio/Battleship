bd3116e3c7d62eed4ce7ec9139062d0ac6a091f9
Justin De Sio <justin.desio@edu.ece.fr>
2022-11-15 15:20:04 +0100

    supprime des trucs inutiles

--- src/main/java/ece/fr/view/ViewCommandLineInterface.java
+++ src/main/java/ece/fr/view/ViewCommandLineInterface.java
@@ -12,22 +12,13 @@ import java.util.Scanner;
 public class ViewCommandLineInterface implements Viewable {
 
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
+
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_PURPLE = "\u001B[35m";
-
     public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
-    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
-    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
-    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
-    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
-    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
     private GameController controller;
@@ -178,7 +169,7 @@ public class ViewCommandLineInterface implements Viewable {
         Scanner scanner = new Scanner(System.in);
         int result;
         try {
-             result = scanner.nextInt();
+            result = scanner.nextInt();
 
             if (result != 1 && result != 2) {
 
