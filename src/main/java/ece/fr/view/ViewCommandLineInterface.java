7044efb09b8a7069ab20ba121273659d846d0bdb
Guiza4 <youssef.guiza@yahoo.com>
2022-11-15 15:33:45 +0100

    Suppression code inutile

--- src/main/java/ece/fr/view/ViewCommandLineInterface.java
+++ src/main/java/ece/fr/view/ViewCommandLineInterface.java
@@ -12,7 +12,6 @@ import java.util.Scanner;
 public class ViewCommandLineInterface implements Viewable {
 
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
